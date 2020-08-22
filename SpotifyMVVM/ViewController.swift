9690bf4b313f5fd3367a998490fceebffa36f06b
Carlos Landaverde <carlosladdz@protonmail.com>
2020-08-22 12:37:51 -0600

    Initial Commit

---
+++ SpotifyMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotifyMVVM
+//
+//  Created by Carlos Landaverde on 8/22/20.
+//  Copyright © 2020 Carlos Landaverde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
